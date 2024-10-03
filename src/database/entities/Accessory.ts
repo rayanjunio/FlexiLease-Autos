2950b02484b4dc86613ab8f32abcbacd3a9da567
Rayan Junio <rayanjunior234@gmail.com>
2024-10-03 18:27:44 -0300

    feat: cria o relacionamento entre carro e acessorios

---
+++ src/database/entities/Accessory.ts
@@ -0,0 +1,20 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+} from "typeorm";
+import { Car } from "./Car";
+
+@Entity("accessories")
+export class Accessory {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  name!: string;
+
+  @ManyToOne(() => Car, (car) => car.accessories)
+  car!: Car;
+}
--- src/database/entities/Car.ts
+++ src/database/entities/Car.ts
@@ -1,10 +1,7 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Reserve } from "./Reserve";
 import { ValidationError } from "../../api/errors/ValidationError";
-
-export interface IAccessory {
-  name: string;
-}
+import { Accessory } from "./Accessory";
 
 @Entity("cars")
 export class Car {
@@ -23,43 +20,12 @@ export class Car {
   @Column()
   valuePerDay!: number;
 
-  @Column("text")
-  accessories!: string;
-
   @Column()
   numberOfPassengers!: number;
 
   @OneToMany(() => Reserve, (reserve) => reserve.carId)
   reserves!: Reserve[];
 
-  getAcessories(): IAccessory[] {
-    return JSON.parse(this.accessories);
-  }
-
-  setAccessories(accessories: IAccessory[]): void {
-    this.accessories = JSON.stringify(accessories);
-  }
-
-  public hasAccessory(accessoryName: string): boolean {
-    const accessoriesArray = this.getAcessories();
-    return accessoriesArray.some((acessory) => acessory.name === accessoryName);
-  }
-
-  public removeAccessory(accessoryName: string): void {
-    const accessoriesArray = this.getAcessories();
-    const updatedAccessories = accessoriesArray.filter(
-      (acessory) => acessory.name !== accessoryName,
-    );
-    this.setAccessories(updatedAccessories);
-  }
-
-  public addAccessory(newAccessory: IAccessory): void {
-    const accessoriesArray = this.getAcessories();
-    if (this.hasAccessory(newAccessory.name)) {
-      const message = `Accessory ${newAccessory.name} already exists.`;
-      throw new ValidationError(400, "Bad Request", message);
-    }
-    accessoriesArray.push(newAccessory);
-    this.setAccessories(accessoriesArray);
-  }
+  @OneToMany(() => Accessory, (accessory) => accessory.car,{ cascade: true })
+  accessories!: Accessory[];
 }
