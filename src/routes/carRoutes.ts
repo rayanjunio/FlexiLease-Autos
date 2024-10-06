b1db8dcc6a753b636d6f394f527022a166ddb1d9
Rayan Junio <rayanjunior234@gmail.com>
2024-10-06 15:32:59 -0300

    feat: adiciona autenticacao nas rotas de carros e usuarios

--- src/routes/carRoutes.ts
+++ src/routes/carRoutes.ts
@@ -1,15 +1,40 @@
 import { Router } from "express";
 import { CarController } from "../api/controllers/CarController";
+import { authMiddleware } from "../api/middlewares/authMiddleware";
 
 const router = Router();
 const carController = new CarController();
 
-router.post("/car", carController.createCar.bind(carController));
-router.get("/car", carController.getAllCars.bind(carController));
+router.post(
+  "/car",
+  authMiddleware,
+  carController.createCar.bind(carController),
+);
+router.get(
+  "/car",
+  authMiddleware,
+  carController.getAllCars.bind(carController),
+);
 
-router.get("/car/:id", carController.getCarById.bind(carController));
-router.put("/car/:id", carController.updateCar.bind(carController));
-router.patch("/car/:id", carController.updateAccessories.bind(carController));
-router.delete("/car/:id", carController.deleteCar.bind(carController));
+router.get(
+  "/car/:id",
+  authMiddleware,
+  carController.getCarById.bind(carController),
+);
+router.put(
+  "/car/:id",
+  authMiddleware,
+  carController.updateCar.bind(carController),
+);
+router.patch(
+  "/car/:id",
+  authMiddleware,
+  carController.updateAccessories.bind(carController),
+);
+router.delete(
+  "/car/:id",
+  authMiddleware,
+  carController.deleteCar.bind(carController),
+);
 
 export default router;
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,13 +1,26 @@
 import { Router } from "express";
 import { UserController } from "../api/controllers/UserController";
+import { authMiddleware } from "../api/middlewares/authMiddleware";
 
 const router = Router();
 const userController = new UserController();
 
 router.post("/user", userController.createUser.bind(userController));
 
-router.get("/user/:id", userController.getUserById.bind(userController));
-router.put("/user/:id", userController.updateUser.bind(userController));
-router.delete("/user/:id", userController.deleteUser.bind(userController));
+router.get(
+  "/user/:id",
+  authMiddleware,
+  userController.getUserById.bind(userController),
+);
+router.put(
+  "/user/:id",
+  authMiddleware,
+  userController.updateUser.bind(userController),
+);
+router.delete(
+  "/user/:id",
+  authMiddleware,
+  userController.deleteUser.bind(userController),
+);
 
 export default router;
