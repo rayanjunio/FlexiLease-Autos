ca355e8af24d92ef797e58bb7f462fc57fd4afbb
Rayan Junio <rayanjunior234@gmail.com>
2025-04-17 10:07:10 -0300

    refactor(CarService): inject redisClient instead of importing directly

--- src/config/instances/carInstances.ts
+++ src/config/instances/carInstances.ts
@@ -5,14 +5,19 @@ import { Accessory } from "../../database/entities/Accessory";
 import { CarService } from "../../api/services/CarService";
 import { AccessoryService } from "../../api/services/AccessoryService";
 import { CarController } from "../../api/controllers/CarController";
+import { RedisClientType } from "redis";
+import { redisClient } from "../redis";
 
 // repositories
 const carRepository: Repository<Car> = AppDataSource.getRepository(Car);
 const accessoryRepository: Repository<Accessory> = AppDataSource.getRepository(Accessory);
 
+// redis
+const redis: RedisClientType = redisClient;
+
 // services
 const accessoryService: AccessoryService = new AccessoryService(accessoryRepository);
-const carService: CarService = new CarService(carRepository, accessoryService, accessoryRepository);
+const carService: CarService = new CarService(carRepository, accessoryService, accessoryRepository, redis);
 
 // controller
 export const carController: CarController = new CarController(carService);
