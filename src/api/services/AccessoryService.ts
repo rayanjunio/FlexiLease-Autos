d6e7102342b2dec4485d028a30523a4389afccb7
Rayan Junio <rayanjunior234@gmail.com>
2025-04-19 10:47:25 -0300

    feat: improve accessory update logic in handleAccessoryUpdate method

--- src/api/services/AccessoryService.ts
+++ src/api/services/AccessoryService.ts
@@ -1,6 +1,7 @@
 import { Repository } from "typeorm";
 import { Accessory } from "../../database/entities/Accessory";
 import { ValidationError } from "../errors/ValidationError";
+import { Car } from "../../database/entities/Car";
 
 export class AccessoryService {
   constructor(private accessoryRepository: Repository<Accessory>) {}
@@ -16,22 +17,31 @@ export class AccessoryService {
     return await this.accessoryRepository.findOne({ where: { id } });
   }
 
-  async updateAccessory(
-    id: number,
-    name: string,
-  ): Promise<Accessory | undefined> {
-    if (!name || name.trim() === "") {
-      throw new Error("Accessory name cannot be empty");
+  async handleAccessoryUpdate(
+    newAccessory: { name: string },
+    car: Car,
+  ): Promise<Accessory | null> {
+    if (!newAccessory || newAccessory.name.trim() === "") {
+      const message: string = "Accessory name cannot be empty";
+      throw new ValidationError(400, "Bad Request", message);
     }
 
-    const accessory = await this.getAccessoryById(id);
+    const accessoryRepeated: Accessory | undefined = car.accessories.find(
+      accessory => accessory.name.includes(newAccessory.name));
 
-    if (!accessory) {
-      throw new Error("Accessory not found");
-    }
+    if(accessoryRepeated) {
+      await this.deleteAccessory(accessoryRepeated.id);
+      return null;
+    }  
 
-    accessory.name = name;
-    return await this.accessoryRepository.save(accessory);
+    let createdAccessory: Accessory = new Accessory();
+
+    createdAccessory.name = newAccessory.name;
+    createdAccessory.car = car;
+
+    await this.accessoryRepository.save(createdAccessory);
+
+    return createdAccessory;
   }
 
   async deleteAccessory(id: number): Promise<void> {
